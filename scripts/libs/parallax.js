3b61a33105ed309031da92b4863c11d2b3608410
shoutaoumaru <otokmae.black@gmail.com>
2021-07-24 11:34:21 +0900

    パララックス、レイアウトデバック

--- scripts/libs/parallax.js
+++ scripts/libs/parallax.js
@@ -1,7 +1,16 @@
 //パララックス縦
-const parallax = document.querySelectorAll('.parallax');
+const parallax = document.querySelectorAll('.parallax-img');
 new simpleParallax(parallax, {
-  scale: 1.3
+  scale: 1.2,
+  delay: 0.4,
+  orientation: 'down'
+});
+const parallax03 = document.querySelectorAll('.parallax');
+new simpleParallax(parallax03, {
+  scale: 1.2,
+  delay: 0.4,
+  overflow: true,
+  orientation: 'down'
 });
 //パララックス横右
 const parallax01 = document.querySelectorAll('.parallax-rt');
@@ -13,15 +22,4 @@ const parallax02 = document.querySelectorAll('.parallax-lt');
 new simpleParallax(parallax02, {
   orientation: 'left',
   scale: 1.3
-});
-
-//* * *  * * *  パララックスbg* *  * * * * * * /
-
-var rellax = new Rellax('.rellax', {
-  speed: -0.7,
-  center: false,
-  wrapper: null,
-  round: true,
-  vertical: true,
-  horizontal: false
 });
