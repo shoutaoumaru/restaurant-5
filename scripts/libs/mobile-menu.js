c67ad08ae9daa071ae87b4c7deaec33ccfe756bd
aikawatakuya <85001312+aikawatakuya@users.noreply.github.com>
2021-09-26 16:48:55 +0900

    モバイルスクロール　画像変更

--- scripts/libs/mobile-menu.js
+++ scripts/libs/mobile-menu.js
@@ -1,21 +1,23 @@
 class MobileMenu {
   constructor() {
     this.DOM = {};
-    this.DOM.btn = document.querySelector('.mobile-menu__btn');
-    this.DOM.container = document.querySelector('.mobile-button');
+    this.DOM.body = document.querySelector("body");
+    this.DOM.btn = document.querySelector(".mobile-menu__btn");
+    this.DOM.container = document.querySelector(".mobile-button");
     // this.DOM.logo = document.querySelector('.logo');
-    this.DOM.nav = document.querySelector('.mobile-menu');
-    this.DOM.link = document.querySelector('.mobile-menu__list');
+    this.DOM.nav = document.querySelector(".mobile-menu");
+    this.DOM.link = document.querySelector(".mobile-menu__list");
     this.eventType = this._getEventType();
     this._addEvent();
   }
 
   _getEventType() {
-    return window.ontouchstart ? 'touchstart' : 'click';
+    return window.ontouchstart ? "touchstart" : "click";
   }
   _toggle() {
-    this.DOM.container.classList.toggle('menu-open');
-    this.DOM.nav.classList.toggle('menu-open');
+    this.DOM.body.classList.toggle("hidden");
+    this.DOM.container.classList.toggle("menu-open");
+    this.DOM.nav.classList.toggle("menu-open");
     // this.DOM.logo.classList.toggle('menu-open');
   }
 
